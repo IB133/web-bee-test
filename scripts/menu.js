4781f5be6d0ecfc71315b84b3c85a0633a50ded9
IB133 <bii.20@uni-dubna.ru>
2023-03-27 01:13:15 +0300

    fix: styles

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,13 +3,13 @@ function menuSelected(item)
 {
     let selected;
     if (currentMenuItem == null){
-        item.classList.add('menu-sel');
+        item.classList.add('menu-selected');
         currentMenuItem = item;
         return
     }
     let prev = currentMenuItem;
-    prev.classList.remove('menu-sel');
+    prev.classList.remove('menu-selected');
     selected = item;
-    selected.classList.add('menu-sel');
+    selected.classList.add('menu-selected');
     currentMenuItem = item;
 }
